3ca6e910a03070baf60794aa2afccde547758349
Cam Luc <cam@hitekdata.com>
2011-11-18 17:02:56 -0500

    Create Models/DinnerRepository.cs

---
+++ DiNerdDinner/Models/DinnerRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DiNerdDinner.Models {
+    public class DinnerRepository {
+
+        private NerdDinnerEntities entities = new NerdDinnerEntities();
+
+        // Query Methods
+        public IQueryable<Dinner> FindAllDinners() {
+            return entities.Dinners;
+        }
+
+        public IQueryable<Dinner> FindUpcomingDinners() {
+            return from dinner in entities.Dinners
+                   where dinner.EventDate > DateTime.Now
+                   orderby dinner.EventDate
+                   select dinner;
+        }
+
+        public Dinner GetDinner(int id) {
+            return entities.Dinners.FirstOrDefault(d => d.DinnerID == id);
+        }
+
+        // Insert/Delete
+        public void Add(Dinner dinner) {
+            entities.Dinners.AddObject(dinner);
+        }
+
+        public void Delete(Dinner dinner) {
+            foreach (var rsvp in dinner.RSVPs) {
+                entities.RSVPs.DeleteObject(rsvp);
+            }
+            entities.Dinners.DeleteObject(dinner);
+        }
+
+        // Persistence
+        public void Save() {
+            entities.SaveChanges();
+        }
+    }
+}
